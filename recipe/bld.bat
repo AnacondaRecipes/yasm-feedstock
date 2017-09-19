46f68aaa07cbcc72855198e3a7b0c27b11bd9b36
Michael Sarahan <msarahan@gmail.com>
2017-09-19 13:37:24 -0500

    use cmake instead of nmake

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 mkdir build
 cd build
 
-cmake -G "NMake Makefiles" ^
+cmake -G "%CMAKE_GENERATOR%" ^
          -DCMAKE_BUILD_TYPE:STRING=RELEASE ^
          -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
          -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
@@ -9,8 +9,5 @@ cmake -G "NMake Makefiles" ^
          %SRC_DIR%
 if errorlevel 1 exit 1
 
-nmake
-if errorlevel 1 exit 1
-
-nmake install
+cmake --build . --target install
 if errorlevel 1 exit 1
